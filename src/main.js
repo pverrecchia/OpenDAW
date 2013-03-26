5645a5548e0fb5832b2e34b9065058575b395d04
Adam Levine <timsoulm@gmail.com>
2013-03-25 23:11:06 -0400

    Implemented Drag N Drop from library samples
    -only creates canvas elements, doesn't set up audio

--- src/main.js
+++ src/main.js
@@ -8,21 +8,24 @@ var buffers = []; //contains AudioBuffer and id# of samples in workspace
 var times = []; //contains start times of samples and their id#
 var pixelsPer16 = 6; 			//pixels per 16th note. used for grid snapping
 var pixelsPer4 = 4*pixelsPer16;		//pixels per 1/4 note	used for sample canvas size
+var bpm = 128;
 	
 	
 var wavesurfer = (function () {
     'use strict';
 
     var createWavesurfer = function (song) {
-        var bpm = 128;
-
         var startTimes = song.startTime;
         var sampleNumber = 0;
         var sampleUrl = song.url.split("/");
         var sampleTitle = sampleUrl[sampleUrl.length-1];
 	var obj;
-        $("#library").append("<li id=librarySample" + song.id +" class=\"librarySample\"><a href=\"#\">" + sampleTitle + "</a></li>");
-        $("#librarySample" + song.id).draggable({ revert: true, helper: "clone" });
+        $("#library").append("<li id=librarySample" + song.id +" class=\"librarySample\" data-id="+song.id+" data-url="+song.url+" data-duration="+song.duration+"><a href=\"#\">" + sampleTitle + "</a></li>");
+        $("#librarySample" + song.id).draggable({
+	    revert: true,
+	    helper: "clone",
+	    start: function(event, ui) { $(this).css("z-index", 10); }
+	});
         $.each(startTimes, function(){
 	    if(sampleNumber == 0){
 		obj = ({bufferURL: song.url, id: song.id, startTimes: song.startTime});
@@ -69,6 +72,48 @@ var wavesurfer = (function () {
 	var numberOfTracks = parseInt(json.projectInfo.tracks);
 	for(var i=0;i<numberOfTracks;i++){
 	    $("#trackBed").append("<div class=\"span9\"><div class=\"row-fluid\"><div class=\"span2 well\" style=\"height: 84px;\"><p>Track"+(i+1)+"</p><div class=\"btn-group\"><button class=\"btn btn-mini\"><i class=\"icon-headphones\"></i></button><button class=\"btn btn-mini\"><i class=\"icon-volume-off\"></i></button><button class=\"btn btn-mini\"><i class=\"icon-plus-sign\"></i></button></div></div><div id=\"track"+(i+1)+"\" class=\"span10 track\"></div></div></div>");
+	    $( "#track"+(i+1) ).droppable({
+		accept: ".librarySample",
+		drop: function( event, ui ) {
+		    var startBar = Math.floor((ui.offset.left-$(this).offset().left)/6);
+		    console.log(startBar);
+		    var span = document.createElement('span');
+		    var sampleID = ui.helper.attr("data-id");
+		    var sampleDuration = ui.helper.attr("data-duration");
+		    var sampleURL = ui.helper.attr("data-url");
+		    span.id = "sample" + sampleID + "Span";
+		    var canvas = document.createElement('canvas');
+		    canvas.id = "sample" + sampleID + "Canvas";
+		    $(this).append(span);
+		    $("#sample" + sampleID + "Span").append(canvas);
+		    $("#sample" + sampleID + "Span").width(parseFloat(sampleDuration) * ((pixelsPer4*bpm)/60));
+		    canvas.width = parseFloat(sampleDuration) * ((pixelsPer4*bpm)/60);
+		    canvas.height = 80;
+		    $( "#sample" + sampleID + "Span").attr('data-startTime',startBar);
+		    $( "#sample" + sampleID + "Span").css('left',"" + startBar*pixelsPer16 + "px");
+		    $( "#sample" + sampleID + "Span").css('position','absolute');
+		    $( "#sample" + sampleID + "Span").draggable({
+			axis: "x",
+			containment: "parent",
+			grid: [pixelsPer16, 0],		//grid snaps to 16th notes
+			stop: function() {
+			    $( "#sample" + song.id + "Span").attr('data-startTime',parseInt($( "#sample" + song.id + "Span").css('left')));
+			}
+		    });
+		    
+		    var wavesurfer = Object.create(WaveSurfer);
+		    wavesurfer.init({
+			canvas: canvas,
+			waveColor: 'violet',
+			progressColor: 'purple',
+			loadingColor: 'purple',
+			cursorColor: 'navy',
+			audioContext: ac
+		    });
+		    wavesurfer.load(sampleURL);
+		    
+		}
+	    });
 	}
 	//wavesurfers is array of all tracks
         var wavesurfers = json.samples.map(createWavesurfer);
@@ -148,9 +193,6 @@ $(document).ready(function(){
     });
     $("#step-backward").click(function(){
         $('body').trigger('stepBackward-event');
-    });
-    $( "#track1" ).droppable({
-        accept: ".librarySample"
     });
     var c=document.getElementById("timeline");
     var ctx=c.getContext("2d");
